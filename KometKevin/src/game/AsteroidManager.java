9c4268c35c85c17b85439f8e2ff07786ccad6917
LANGARITA BENITEZ, SERGIO <718121@celes.unizar.es>
2019-05-07 11:57:28 +0200

    Replace AsteroidManager.java

--- KometKevin/src/game/AsteroidManager.java
+++ KometKevin/src/game/AsteroidManager.java
@@ -20,56 +20,54 @@ public class AsteroidManager {
     private Config config;
     private Random random;
     
+    private double counter = 0;
+    
     public AsteroidManager(Camera camera,GameManager gm){  
         this.camera=camera;
         this.gm=gm;
         this.config=Config.getInstance();
-
         this.random=new Random();
     }
     
     public void generateAsteriods(float dt){   
-        if(gm.getObjects().size()<500){
-            int horizontal=generarHorizontal();
-            int vertical=generarvertical();
-
+        if(gm.getObjects().size()<500){ 
+            int x=(int)camera.getOffX();
+            int horizontal=random.nextInt(gm.getBackground().getW());
+            if(horizontal>x-50 && horizontal<x+config.getScreenWidth()+50){
+                horizontal= -1;
+            }
+            int y=(int)camera.getOffY();
+            int vertical=random.nextInt(gm.getBackground().getH());
+            if(vertical>y-50 && vertical<y+config.getScreenHeight()+50){
+                vertical=-1;
+            }  
             if(horizontal!=-1 && vertical!=-1){
                 Asteroid kevin= new Asteroid(horizontal,vertical);
-                double x=2+random.nextDouble();
-                double y=2+random.nextDouble();
-                int direction=random.nextInt(4);
-                if(direction==0){
-                    x=-x;
-                    y=-y;
-                }else if(direction==1){
-                    x=-x;
-                }else if(direction==2){
-                    y=-y;
+                if (counter %(3*dt)==0) {
+                    Vector2 aux= new Vector2(x,y);
+                    aux.subtract(kevin.getCenter());
+                    //aux = kevin.getCenter().getSubtracted(aux)
+                    kevin.setVelocity(Vector2.toCartesian(9, aux.getAngle()));
+                } else {
+                    double velx=2+random.nextDouble();
+                    double vely=2+random.nextDouble();
+                    int direction=random.nextInt(4);
+                    if(direction==0){
+                        velx=-velx;
+                        vely=-vely;
+                    }else if(direction==1){
+                        velx=-velx;
+                    }else if(direction==2){
+                        vely=-vely;
+                    }
+                    kevin.setVelocity(new Vector2(velx, vely));
+                    //gm.getObjects().add(kevin);
                 }
-                kevin.setVelocity(new Vector2(x, y));
                 gm.getObjects().add(kevin);
             }
         }
-
-    }
-    private int generarHorizontal(){
-        int x=(int)camera.getOffX();
-        int anchura=config.getScreenWidth(); //anchura
-        int screenW=gm.getBackground().getW(); //Anchura total de la pantalla 
-        int horizontal=random.nextInt(screenW);
-        if(horizontal>x-50 && horizontal<x+anchura+50){
-            return -1;
-        }
-        return horizontal;
-    }
-    private int generarvertical(){
-        int y=(int)camera.getOffY();
-        int altura=config.getScreenHeight(); //altura   
-        int screenH=gm.getBackground().getH(); //Altura total de la pantalla
-        int vertical=random.nextInt(screenH);
-        if(vertical>y-50 && vertical<y+altura+50){
-            return -1;
-        }
-        return vertical;
-    }
+    counter += dt;
+    } 
 }
+    
+
